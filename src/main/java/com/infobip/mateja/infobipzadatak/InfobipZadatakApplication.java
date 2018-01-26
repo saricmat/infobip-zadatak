8dbfc3f5142b2cfa48e5c68c51f532801ed858cf
saricmat <mateja2292@gmail.com>
2018-01-26 09:02:43 +0100

    dodavanje spring projekta

---
+++ src/main/java/com/infobip/mateja/infobipzadatak/InfobipZadatakApplication.java
@@ -0,0 +1,12 @@
+package com.infobip.mateja.infobipzadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfobipZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfobipZadatakApplication.class, args);
+	}
+}
---
+++ src/test/java/com/infobip/mateja/infobipzadatak/InfobipZadatakApplicationTests.java
@@ -0,0 +1,16 @@
+package com.infobip.mateja.infobipzadatak;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfobipZadatakApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
